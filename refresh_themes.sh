8b33d49ca6bafae3f049e18b3d94c86d256b142a
Andy Taylor <krumphau@gmail.com>
2020-05-04 16:41:58 +0100

    refreshThemes.sh

---
+++ refresh_themes.sh
@@ -0,0 +1,5 @@
+git pull
+
+docker cp ./piani/piani_theme/ theanderenone_wordpress-piani_1:/var/www/html/wp-content/themes/piani/
+docker cp ./piani/piani_theme/ theanderenone_wordpress-bibmundo_1:/var/www/html/wp-content/themes/bibundo/
+docker cp ./piani/piani_theme/ theanderenone_wordpress-pogrom_1:/var/www/html/wp-content/themes/pogrom/
