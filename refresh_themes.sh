90f5aae1b9fa3c2e9703631f9b1bfecc7f37d432
Andy Taylor <krumphau@gmail.com>
2020-05-04 18:54:27 +0100

    rm -r

--- refresh_themes.sh
+++ refresh_themes.sh
@@ -1,6 +1,6 @@
 git pull
 
-docker exec -it theanderenone_wordpress-piani_1 rm /var/www/html/wp-content/themes/piani
+docker exec -it theanderenone_wordpress-piani_1 rm -r /var/www/html/wp-content/themes/piani
 docker cp ./piani/piani_theme theanderenone_wordpress-piani_1:/var/www/html/wp-content/themes/piani
 docker cp ./bibmundo/bibmundo_theme theanderenone_wordpress-bibmundo_1:/var/www/html/wp-content/themes/bibmundo
 docker cp ./pogrom/pogrom_theme theanderenone_wordpress-pogrom_1:/var/www/html/wp-content/themes/pogrom
