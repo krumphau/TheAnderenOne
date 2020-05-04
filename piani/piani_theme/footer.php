fabeae1c17c9534edf6ed05bc055acad009b492c
Andy Taylor <krumphau@gmail.com>
2020-05-04 17:25:58 +0100

    ...

--- piani/piani_theme/footer.php
+++ piani/piani_theme/footer.php
@@ -13,12 +13,11 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-		
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
 
 </body>
 </html>
