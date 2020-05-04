f3be2919a87d44563cabc6050d1184c2a195d4ee
Andy Taylor <krumphau@gmail.com>
2020-05-04 16:49:22 +0100

    source added for pogrom and bibmundo themes

--- refresh_themes.sh
+++ refresh_themes.sh
@@ -1,5 +1,5 @@
 git pull
 
 docker cp ./piani/piani_theme/ theanderenone_wordpress-piani_1:/var/www/html/wp-content/themes/piani/
-docker cp ./piani/piani_theme/ theanderenone_wordpress-bibmundo_1:/var/www/html/wp-content/themes/bibundo/
-docker cp ./piani/piani_theme/ theanderenone_wordpress-pogrom_1:/var/www/html/wp-content/themes/pogrom/
+docker cp ./bibmundo/bibmundo_theme/ theanderenone_wordpress-bibmundo_1:/var/www/html/wp-content/themes/bibundo/
+docker cp ./pogrom/pogrom_theme/ theanderenone_wordpress-pogrom_1:/var/www/html/wp-content/themes/pogrom/
