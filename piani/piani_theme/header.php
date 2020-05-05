6054692985c40fd5fdb8fa8f3713b0dabc1c398a
Andy Taylor <krumphau@gmail.com>
2020-05-05 19:50:39 +0100

    menu button css

--- piani/piani_theme/header.php
+++ piani/piani_theme/header.php
@@ -31,29 +31,11 @@
       <div class="flex-body">
 				<div class="flex-column">
 				<!-- Collapse button -->
- <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-	 aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-			 class="fas fa-bars fa-1x"></i></span></button>
+          <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+	           aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+			       class="fas fa-bars fa-1x"></i></span></button>
 
- <!-- Collapsible content -->
- <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-
-	 <!-- Links -->
-	 <ul class="navbar-nav mr-auto">
-		 <li class="nav-item active">
-			 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		 </li>
-		 <li class="nav-item">
-			 <a class="nav-link" href="#">Features</a>
-		 </li>
-		 <li class="nav-item">
-			 <a class="nav-link" href="#">Pricing</a>
-		 </li>
-	 </ul>
-	 <!-- Links -->
-
- </div>
-</div>
+        </div>
  <!-- Collapsible content -->
 				<div class="flex-column">
 			<?php
