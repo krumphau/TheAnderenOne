16e10cbc3b1ebb8e93ed0b568296ce6399821cb3
Andy Taylor <krumphau@gmail.com>
2020-05-06 20:17:36 +0100

    single post layout

--- piani/piani_theme/single.php
+++ piani/piani_theme/single.php
@@ -9,7 +9,9 @@
 
 get_header();
 ?>
-
+<div class="container">
+	<div class="row">
+		<div class="col">
 	<main id="primary" class="site-main">
 
 		<?php
@@ -34,7 +36,13 @@ get_header();
 		?>
 
 	</main><!-- #main -->
-
+</div>
+<div class="col">
 <?php
 get_sidebar();
+?>
+</div>
+</div>
+</div>
+<?php 
 get_footer();
