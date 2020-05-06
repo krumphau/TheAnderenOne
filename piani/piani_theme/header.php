1c5bc60131bfef176f7c35a4f7cc963df5bcb78f
Andy Taylor <krumphau@gmail.com>
2020-05-06 11:56:48 +0100

    menu button

--- piani/piani_theme/header.php
+++ piani/piani_theme/header.php
@@ -32,7 +32,7 @@
 				<div class="flex-column">
 				<!-- Collapse button -->
           <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-	           aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+	           aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="white-text"><i
 			       class="fas fa-bars fa-1x"></i></span></button>
 
         </div>
