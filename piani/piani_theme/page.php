2b30b43d5280f14665e825893ff8d68b8db5535c
Andy Taylor <krumphau@gmail.com>
2020-05-06 13:00:44 +0100

    ...

--- piani/piani_theme/page.php
+++ piani/piani_theme/page.php
@@ -14,7 +14,7 @@
 
 get_header();
 ?>
-<div style="padding-left:15px;">
+
 	<main id="primary" class="site-main">
 
 		<?php
@@ -32,7 +32,7 @@ get_header();
 		?>
 
 	</main><!-- #main -->
-</div>
+
 <?php
 get_sidebar();
 get_footer();
