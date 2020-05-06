5659d4e66848a48b366d3bc9297a19a221684601
Andy Taylor <krumphau@gmail.com>
2020-05-06 19:14:28 +0100

    final art - spacing -changes

--- piani/piani_theme/header.php
+++ piani/piani_theme/header.php
@@ -22,22 +22,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<script>$
-	(document).ready(function () {
-
-  $('.first-button').on('click', function () {
-
-    $('.animated-icon1').toggleClass('open');
-  });
-  $('.second-button').on('click', function () {
-
-    $('.animated-icon2').toggleClass('open');
-  });
-  $('.third-button').on('click', function () {
-
-    $('.animated-icon3').toggleClass('open');
-  });
-}); </script>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'piani_theme' ); ?></a>
@@ -45,53 +29,18 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
       <div class="flex-body">
-				<div class="flex-column">
-				<!--Navbar-->
-<nav class="navbar navbar-light amber lighten-4 mb-4">
 
-  <!-- Navbar brand -->
-  <a class="navbar-brand" href="#">Navbar</a>
-
-  <!-- Collapse button -->
-  <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20"
-    aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
-    <div class="animated-icon1"><span></span><span></span><span></span></div>
-  </button>
-
-  <!-- Collapsible content -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent20">
-
-    <!-- Links -->
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-    <!-- Links -->
-
-  </div>
-  <!-- Collapsible content -->
-
-</nav>
-<!--/.Navbar-->
-</div>
 				<div class="flex-column">
 			<?php
 			if ( is_front_page() && is_home() ) :
 				?>
-      <div style="margin-left: 30px;">
+      <div style="margin-left: 48px;">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			</div>
 				<?php
 			else :
 				?>
-      <div style="margin-left: 30px;">
+      <div style="margin-left: 48px;">
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			</div>
 		</div>
@@ -100,13 +49,13 @@
 			$piani_theme_description = get_bloginfo( 'description', 'display' );
 			if ( $piani_theme_description || is_customize_preview() ) :
 				?>
-				<div style="margin-left: 30px;">
+				<div style="margin-left: 48px;">
 				<div><p class="site-description"><?php echo $piani_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
         </div>
 				</div>
 			</div>
 			<div class="flex-row">
-				<div style='float: right;'><img style = 'float:right; padding-right: 30px; margin-top: 30px; height:74px;'  src = "http://3.15.13.29:8000/wp-content/uploads/2020/05/logotype_white@2x-1-e1588619045637.png"/></div>
+				<div style='float: right;'><img style = 'float:right; padding-right: 48px; margin-top: 48px; height:74px;'  src = "http://3.15.13.29:8000/wp-content/uploads/2020/05/logotype_white@2x-1-e1588619045637.png"/></div>
       </div>
 			</div><!--.end flex-body-->
 			<?php endif; ?>
