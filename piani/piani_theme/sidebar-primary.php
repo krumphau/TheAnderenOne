1532b14d7571290319b7c6e58d7ecfd40169f79d
Andy Taylor <krumphau@gmail.com>
2020-05-10 20:54:37 +0100

    ...

--- piani/piani_theme/sidebar-primary.php
+++ piani/piani_theme/sidebar-primary.php
@@ -1,4 +1,5 @@
 <div id="sidebar-primary" class="sidebar">
+<div>
     <h2 >Orari di Apertura</h2>
     <ul style='list-style-type:none; padding-left: 0px; margin-left: 0px;'>
     <li>Lunedi: 15-18</li>
@@ -13,7 +14,7 @@
 <div>
   <img src="http://3.15.13.29:8000/wp-content/themes/piani/images/illustrations-01.png"/>
 </div>
-<div class="sidebar">
+<div>
     <h2 >Il gruppo culturale</h2>
     <p>
       Il Gruppo Culturale organizza varie attività quali dibattiti,
@@ -25,9 +26,10 @@
 <div>
   <img src="http://3.15.13.29:8000/wp-content/themes/piani/images/illustrations-02.png"/>
 </div>
-<div class="sidebar">
+<div>
 <h2>Pianissmo</h2>
 <p>la pubblicazione del giornale è un attività che fortemente impegna l’Associazione
-  Biblioteca Piani: i collaboratori volontari raccolgono e scrivono gli articoli, impaginano 
+  Biblioteca Piani: i collaboratori volontari raccolgono e scrivono gli articoli, impaginano
   il giornalino, lo stampano con il ciclostile e lo rilegano… a mano (che costanza!).</p>
-</div
+</div>
+</div>
