3801470349c56ebb17410351154408c521e03242
Andy Taylor <krumphau@gmail.com>
2020-05-10 20:42:32 +0100

    ...

--- piani/piani_theme/footer.php
+++ piani/piani_theme/footer.php
@@ -14,7 +14,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-6">
-      <h6 style="color:#ffffff; padding-top:17px;">Cerca catalogo BIS</h6>
+      <h6 style="color:#ffffff; padding-top:20px;">Cerca catalogo BIS</h6>
     </div>
     <div class ="col-6">
 			<img src="http://3.15.13.29:8000/wp-content/themes/piani/images/arrow.png" height="48" style="float:right; padding-top: 10px; padding-bottom: 10px;" >
