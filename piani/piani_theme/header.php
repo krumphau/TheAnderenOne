33e0a055cb1d6807d9d86bc947ea8400e63115a8
Andy Taylor <krumphau@gmail.com>
2020-05-10 15:26:25 +0100

    ...

--- piani/piani_theme/header.php
+++ piani/piani_theme/header.php
@@ -49,7 +49,7 @@
 
 			</div>
 			<div class="col-6">
-				<div style = 'float: right;'><img style = 'float: right; padding-top:10%; height:74px;'  src = "http://3.15.13.29:8000/wp-content/uploads/2020/05/logotype_white@2x-1-e1588619045637.png"/></div>
+				<div style = 'float: right;'><img style = 'float: right; margin: 0.67em 0; height:74px;'  src = "http://3.15.13.29:8000/wp-content/uploads/2020/05/logotype_white@2x-1-e1588619045637.png"/></div>
       </div>
 
 			</div><!--.end flex-body-->
